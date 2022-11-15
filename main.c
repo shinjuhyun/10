e3cc9e1366e7c3d9efc5c881283636c022ee89de
shinjuhyun <112927859+shinjuhyun@users.noreply.github.com>
2022-11-15 17:06:55 +0900

    05

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int main(void) {
-	int i = 100;
-	int *p = &i;
-	int **q = &p;
 	
-	*p = 200;
-	printf("i = %d, *p = %d, **q = %d\n",i,*p,**q);
-	
-	**q = 300;
-	printf("i = %d, *p = %d, **q = %d\n",i,*p,**q);
-	
+	char *pc;
+	int *pi;
+	double *pd;
+	pc = (char *)10000;
+	pi = (int*)10000;
+	pd = (double *)10000;
+	printf("before increase : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
+	pc++;
+	pi++;
+	pd++;
+	printf("after increase : pc = %d, pi = %d, pd = %d\n", pc, pi, pd);
 	return 0;
 }
